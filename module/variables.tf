61d18c9d22145d2bb1b4e388f6bd6bf0b3cb1d40
Evgeniy Kulynych <evgeniy@goodcode.us>
2023-04-20 08:56:23 +0300

    Update variables.tf

--- module/variables.tf
+++ module/variables.tf
@@ -1,34 +1,27 @@
 variable "create" {
   description = "Whether to create this resource or not?"
   type        = bool
-  default     = true
-  required    = true
 }
 
 variable "bucket" {
   description = "The name of the bucket to put the file in. Alternatively, an S3 access point ARN can be specified."
   type        = string
   default     = ""
-  required    = false
 }
 
 variable "key" {
   description = "The name of the object once it is in the bucket."
   type        = string
   default     = ""
-  required    = true
 }
 
 variable "defaults" {
   description = "Map of default values which will be used for each item."
   type        = any
-  default     = null
-  required    = false
 }
 
 variable "items" {
   description = "Maps of items to create a wrapper from. Values are passed through to the module."
   type        = any
   default     = false
-  required    = true
 }
