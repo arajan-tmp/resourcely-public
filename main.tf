3ce5812a52ccf6aedbace8e053a3636f24b1ba54
Evgeniy Kulynych <evgeniy@goodcode.us>
2024-03-07 10:37:55 +0200

    [Resourcely Instance] qwe

--- main.tf
+++ main.tf
@@ -54,3 +54,60 @@ resource "aws_s3_bucket_lifecycle_configuration" "resourcely-test_m9x6jDJtmwyscV
     }
   }
 }
+
+resource "aws_s3_bucket" "resourcely-asd_qkV23Ls3ZsAwxTiB" {
+  bucket = "resourcely-asd"
+}
+
+resource "aws_s3_bucket_public_access_block" "resourcely-asd_qkV23Ls3ZsAwxTiB" {
+  bucket                  = aws_s3_bucket.resourcely-asd_qkV23Ls3ZsAwxTiB.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "resourcely-asd_qkV23Ls3ZsAwxTiB" {
+  bucket = aws_s3_bucket.resourcely-asd_qkV23Ls3ZsAwxTiB.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "resourcely-asd_qkV23Ls3ZsAwxTiB" {
+  bucket = aws_s3_bucket.resourcely-asd_qkV23Ls3ZsAwxTiB.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "resourcely-asd_qkV23Ls3ZsAwxTiB" {
+  bucket = aws_s3_bucket.resourcely-asd_qkV23Ls3ZsAwxTiB.id
+
+  rule {
+    id     = "transition-to-glacier"
+    status = "Enabled"
+
+    filter {
+    }
+
+    transition {
+      days          = 123
+      storage_class = "GLACIER"
+    }
+  }
+
+  rule {
+    id     = "abort-multipart-upload"
+    status = "Enabled"
+
+    filter {
+    }
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+  }
+}
